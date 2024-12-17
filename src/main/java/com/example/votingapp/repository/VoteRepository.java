303ba0044bc5eef918dd2facf47d7d9cd273d1e2
청소부장 <washingmanager2@gmail.com>
2024-12-17 20:12:25 +0900

    Create VoteRepository.java

---
+++ src/main/java/com/example/votingapp/repository/VoteRepository.java
@@ -0,0 +1,7 @@
+package com.example.votingapp.repository;
+
+import com.example.votingapp.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+}
